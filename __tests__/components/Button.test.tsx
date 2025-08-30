c6251b19066b803b1588bb1a570bd6e1465d229e
Ionathan Sideras <iona8ansideras@gmail.com>
2025-08-30 15:55:19 +0300

    commit test icon in button

--- __tests__/components/Button.test.tsx
+++ __tests__/components/Button.test.tsx
@@ -5,31 +5,39 @@ import userEvent from "@testing-library/user-event";
 
 describe("Button", () => {
     it("renders button as link if it has href", () => {
-        render(<Button href="/test">test</Button>);
+        render(
+            <Button href="/test" icon={<div>icon</div>}>
+                test
+            </Button>
+        );
 
         const link = screen.getByRole("link");
         const button = screen.queryByRole("button");
+        const icon = screen.getByText("icon");
 
         expect(link).toBeInTheDocument();
         expect(link).toHaveTextContent("test");
         expect(link).toHaveAttribute("href", "/test");
+        expect(icon).toBeInTheDocument();
 
         expect(button).not.toBeInTheDocument();
     });
 
     it("renders button if it doesn't have href", () => {
-        render(<Button>test</Button>);
+        render(<Button icon={<div>icon</div>}>test</Button>);
 
         const button = screen.queryByRole("button");
         const link = screen.queryByRole("link");
+        const icon = screen.getByText("icon");
 
         expect(button).toBeInTheDocument();
         expect(button).toHaveTextContent("test");
+        expect(icon).toBeInTheDocument();
 
         expect(link).not.toBeInTheDocument();
     });
 
-    it("if button clicked then check if function has been called", async () => {
+    it("if button clicked then check if function has been called.", async () => {
         const onClickMock = jest.fn();
         const user = userEvent.setup();
 
