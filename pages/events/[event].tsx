a57d77ffa553195043a7fa7bc67d7faf1b13cc69
Ionathan Sideras <iona8ansideras@gmail.com>
2025-09-12 22:57:37 +0300

    fix bug

--- pages/events/[event].tsx
+++ pages/events/[event].tsx
@@ -2,50 +2,50 @@ import type { GetStaticPropsContext } from "next";
 import Title from "@/components/Title";
 import type { Event } from "@/types";
 
-export default function Event({ event }: { event: Event }) {
+export default function Event() {
     return (
         <div className="under-development">
             <Title>page under development</Title>
-            event: {event.name}
+            {/* event: {event.title} */}
         </div>
     );
 }
 
-export async function getStaticProps(context: GetStaticPropsContext) {
-    const eventId = Number(context.params?.event);
+// export async function getStaticProps(context: GetStaticPropsContext) {
+//     const eventId = Number(context.params?.event);
 
-    const events: Event[] = [
-        { id: 1, name: "test1" },
-        { id: 2, name: "test2" },
-        { id: 3, name: "test3" },
-        { id: 4, name: "test4" },
-    ];
+//     const events: Event[] = [
+//         { id: 1, title: "test1" },
+//         { id: 2, title: "test2" },
+//         { id: 3, title: "test3" },
+//         { id: 4, title: "test4" },
+//     ];
 
-    const event = events.find((event) => eventId === event.id);
+//     const event = events.find((event) => eventId === event.id);
 
-    if (!event) {
-        return {
-            notFound: true,
-        };
-    }
+//     if (!event) {
+//         return {
+//             notFound: true,
+//         };
+//     }
 
-    return { props: { event }, revalidate: 10 };
-}
+//     return { props: { event }, revalidate: 10 };
+// }
 
-export async function getStaticPaths() {
-    const events: Event[] = [
-        { id: 1, name: "test1" },
-        { id: 2, name: "test2" },
-        { id: 3, name: "test3" },
-        { id: 4, name: "test4" },
-    ];
+// export async function getStaticPaths() {
+//     const events: Event[] = [
+//         { id: 1, title: "test1" },
+//         { id: 2, title: "test2" },
+//         { id: 3, title: "test3" },
+//         { id: 4, title: "test4" },
+//     ];
 
-    const paths = events.map((e) => ({
-        params: { event: String(e.id) },
-    }));
+//     const paths = events.map((e) => ({
+//         params: { event: String(e.id) },
+//     }));
 
-    return {
-        paths,
-        fallback: "blocking",
-    };
-}
+//     return {
+//         paths,
+//         fallback: "blocking",
+//     };
+// }
