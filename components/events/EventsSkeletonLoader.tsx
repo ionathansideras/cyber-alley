e3de98675b22085cbe071df7272790e3ad572845
Ionathan Sideras <iona8ansideras@gmail.com>
2025-09-17 01:00:55 +0300

    remove span from loader

--- components/events/EventsSkeletonLoader.tsx
+++ components/events/EventsSkeletonLoader.tsx
@@ -4,7 +4,6 @@ import styles from "@/styles/events/EventsSkeletonLoader.module.css";
 export default function EventsSkeletonLoader() {
     return (
         <section className={styles.wrapper}>
-            <span></span>
             <div className={styles.eventPreviewContainer}>
                 <article></article>
             </div>
